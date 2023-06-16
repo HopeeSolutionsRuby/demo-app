8561ae376f5f0a1ba352786bc24cd7f7c350aa82
longhoangcoder <hoanglongly942@gmail.com>
2023-06-16 17:51:33 +0700

    Callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   has_many :posts, class_name: 'Post'
   around_save :log_save
+  before_validation :normalize_name
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   def log_save
     puts "Before saving the user"
     yield
     puts "After saving the user"
+  end
+  def normalize_name
+  	self.name = name.downcase if name.present?
   end
 end
