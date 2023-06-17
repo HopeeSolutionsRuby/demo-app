a7472123d73750153bd45b9b6d180efea5ed3031
vy <hoanghavy0207@gmail.com>
2023-06-17 23:59:50 +0700

    Practice Callbacks

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,12 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   validates_associated :products
+
+  after_touch :touch_product_or_category
+
+  private
+
+  def touch_product_or_category
+    Rails.logger.debug 'after_touch Category/Product'
+  end
 end
