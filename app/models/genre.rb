d24f03787ffa958e589dfe09666a4734f83bad00
Priya <priyarekahjoyce@gmail.com>
2023-06-21 09:30:19 +0700

    first commit

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,11 @@
 class Genre < ApplicationRecord
 	has_many :songs
 	has_many :artists, through: :songs
- 
+
+	after_touch :log_genre 
+
+	private 
+	def log_genre
+		puts "This genre was touched"
+	end  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,10 @@ class Song < ApplicationRecord
 	belongs_to :artist 
 	belongs_to :genre
 
+	after_create :create_song
+
+	def create_song
+		puts "Song created" 
+	end 
  
 end
