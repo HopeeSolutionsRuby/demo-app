eeaac9baabc8ab6c5ac4536c34cb8abde5cbf617
Tupinkyy <tupinkyy@gmail.com>
2023-06-23 15:31:53 +0700

    add self-refential association categoy

---
+++ app/models/category.rb
@@ -0,0 +1,6 @@
+class Category < ApplicationRecord
+  has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  belongs_to :parent, class_name: 'Category', optional: true
+
+  scope :subcate, ->(category) { where(parent_id: category) }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_025614) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_080617) do
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "first_name", default: ""
     t.string "last_name", default: ""
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_025614) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.bigint "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "faculties", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
