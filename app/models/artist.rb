13d17d0d15b6dc52d784d21347db3392d05c3be2
Priya <priyarekahjoyce@gmail.com>
2023-06-27 16:37:22 +0700

    first commit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,11 @@ class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :genre, through: :songs
 
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id name updated_at]
+  end
+
   def get_genre_of_first_song
     # return the genre of the artist's first saved song
     songs.first
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class Song < ApplicationRecord
-  belongs_to :artist, counter_cache: true
+
+  belongs_to :artist, counter_cache: true 
   belongs_to :genre
 
   after_create :create_song
+
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at id name updated_at]
+  end
 
   def create_song
     Rails.logger.debug 'Song created'
