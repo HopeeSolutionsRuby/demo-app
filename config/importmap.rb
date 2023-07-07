7155afba0a9154cc1d24d56fbe253c874cc5565a
xuanduc <nxduc0703@gmail.com>
2023-07-07 13:09:48 +0700

    issue_10: fix form helper in login_form completed

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,5 @@
-# frozen_string_literal: true
-
 # Pin npm packages by running ./bin/importmap
 
-pin 'application', preload: true
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "application", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
