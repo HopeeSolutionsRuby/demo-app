f9cef03f9d6106efd9b1829b1225aeabfdffc726
Tupinkyy <tupinkyy@gmail.com>
2023-07-12 15:06:18 +0700

    Issue 13: Add accounts model, pagination, search and simple table for admin accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,4 +10,13 @@ class Account < ApplicationRecord
   enum role: { employee: 0, employer: 1, admin: 2 }
 
   mount_uploader :avatar, AvatarUploader
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[avatar created_at email encrypted_password id phone remember_created_at
+       reset_password_sent_at reset_password_token role updated_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    []
+  end
 end
