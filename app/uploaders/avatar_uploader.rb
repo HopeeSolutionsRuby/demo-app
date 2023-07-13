0bbbec1f9f0e5e40aee1e56dcdd0c4924039459b
xuanduc <nxduc0703@gmail.com>
2023-07-13 09:26:18 +0700

    issue_13: implement sign up function, update/destroy accounts for admin

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,19 +5,19 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  version :medium_thumb do
-    process resize_to_fill: [250, 250]
-    def full_filename(_for_file)
-      "avatar_medium.#{file.extension}"
-    end
-  end
+  # version :medium_thumb do
+  #   process resize_to_fill: [250, 250]
+  #   def full_filename(_for_file)
+  #     "avatar_medium.#{file.extension}"
+  #   end
+  # end
 
-  version :small_thumb do
-    process resize_to_fill: [50, 50]
-    def full_filename(_for_file)
-      "avatar_small.#{file.extension}"
-    end
-  end
+  # version :small_thumb do
+  #   process resize_to_fill: [50, 50]
+  #   def full_filename(_for_file)
+  #     "avatar_small.#{file.extension}"
+  #   end
+  # end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Faker::Config.locale = 'vi'
 Account.destroy_all
 
 10.times do
-  password = "123456"
+  password = '123456'
   phone = Faker::PhoneNumber.unique.cell_phone_in_e164
   email = Faker::Internet.unique.email(domain: 'gmail.com')
 
