9c83e0fadb6b505a96d223278e65bd4b9c117843
Tupinkyy <tupinkyy@gmail.com>
2023-07-14 14:35:02 +0700

    Issue 19: Init Employer model

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 20_230_714_064_836) do
-
   create_table 'accounts', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
