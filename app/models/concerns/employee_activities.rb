05054e532ae24ddb87bcc594b7b93fdaa0c43952
jjjsss3 <hoanghavy0207@gmail.com>
2023-07-16 00:01:23 +0700

    Issue 18: Fix conflict and optimize views

--- app/models/concerns/employee_activities.rb
+++ app/models/concerns/employee_activities.rb
@@ -8,6 +8,23 @@ module EmployeeActivities
     after_create :send_email_register
     capitalize_attribute :first_name
     capitalize_attribute :last_name
+
+    def self.create_with_account(params)
+      employee = nil
+      ActiveRecord::Base.transaction do
+        employee = Employee.new(first_name: params[:first_name], last_name: params[:last_name],
+                                address: params[:address])
+        # Use build_account instead of creating an Account with Account.create! and assign to employee.account.
+        employee.build_account(email: params[:email], password: params[:password], phone: params[:phone],
+                               avatar: params[:avatar])
+        employee.save!
+      end
+
+      employee
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.debug e.message
+      nil
+    end
   end
 
   private
