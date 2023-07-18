142aee2623ce061d287cdb1a48e4e293a7d049d3
Tupinkyy <tupinkyy@gmail.com>
2023-07-18 09:22:14 +0700

    Issue 13: Fix phone regEx and show form

--- app/validators/phone_validator.rb
+++ app/validators/phone_validator.rb
@@ -2,7 +2,7 @@
 
 class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value =~ /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/ || value.blank?
+    return if value =~ /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/ || value.blank?
 
     record.errors.add attribute, (options[:message] || 'is not phone')
   end
