a610412255d365b19cf51569cdb14f6a2d1e722c
Priya <priyarekahjoyce@gmail.com>
2023-07-20 16:05:15 +0700

    Implemented Job tag

--- app/controllers/admins/job_tags_controller.rb
+++ app/controllers/admins/job_tags_controller.rb
@@ -10,44 +10,49 @@ module Admins
       render 'admins/job_tags/index'
     end
 
-    def show; end
+    def show
+      return if @job_tag
+
+      flash[:error] = "Not found your path with id #{params[:id]}"
+      redirect_to admins_job_tags_path
+    end
 
     def new
       @job_tag = JobTag.new
     end
 
     def edit
-      @job_tags = JobTag.all
+      return if @job_tag
+
+      flash[:error] = "Not found your path with id #{params[:id]}"
+      redirect_to admins_job_tags_path
     end
 
-    # POST /job_tags or /job_tags.json
-    # def create
-    #   @job_tag = JobTag.new(job_tag_params)
+    # POST
+    def create
+      job_tag = nil
+      ActiveRecord::Base.transaction do
+        job_tag = JobTag.new(job_tag_params)
+        job_tag.save!
+      end
+      redirect_to admins_job_tags_path, notice: 'Job tag was successfully created.'
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.logger.debug e.message
+      flash[:notice] = 'Invalid job tag information'
+      render :new, status: :unprocessable_entity
+    end
 
-    #   respond_to do |format|
-    #     if @job_tag
-    #       format.html { redirect_to admins_job_tags_path, notice: "Job tag was successfully created." }
-    #       format.json { render json: @job_tag, status: :created }
-    #     else
-    #       flash[:notice] = 'invalid'
-    #       format.html { render :new, status: :unprocessable_entity }
-    #       format.json { render json: @job_tag.errors, status: :unprocessable_entity }
-    #     end
-    #   end
-    # end
+    def update
+      if @job_tag.update(job_tag_params_update)
+        flash[:success] = 'Job tag was successfully updated!'
+        redirect_to admins_job_tags_path
+      else
+        flash[:error] = @job_tag.errors.full_messages
+        redirect_to edit_admins_job_tag_path
+      end
+    end
 
-    # # PATCH/PUT /job_tags/1 or /job_tags/1.json
-    # def update
-    #   if @job_tag.update(job_tag_params_update)
-    #     redirect_to admins_job_tags_path, notice: 'Job tag was successfully updated'
-    #   else
-    #     Rails.logger.debug @job_tag.errors.full_messages
-    #     flash[:notice] = 'Invalid job tag information'
-    #     render :edit
-    #   end
-    # end
-
-    # DELETE /job_tags/1 or /job_tags/1.json
+    # DELETE
     def destroy
       @job_tag.destroy
       redirect_to admins_job_tags_path, notice: 'Job Tag was successfully deleted'
@@ -55,7 +60,6 @@ module Admins
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_job_tag
       @job_tag = JobTag.find(params[:id])
     end
@@ -64,9 +68,8 @@ module Admins
       params.require(:job_tag).permit(:country, :job_id, :job_category_id, :description)
     end
 
-    # Only allow a list of trusted parameters through.
     def job_tag_params
-      params.require(:job_tag).permit(:country, :description)
+      params.require(:job_tag).permit(:country, :job_id, :job_category_id, :description)
     end
   end
 end
