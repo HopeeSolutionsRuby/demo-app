47640f4355d9602f2cc8c8d14678bd170e559e3c
khoadev2512 <khoa.nguyencse.npng@hcmut.edu.vn>
2023-12-04 15:52:09 +0700

    [Issue 31]: Implement Left Sidebar

--- app/controllers/administrators_controller.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class AdministratorsController < ApplicationController
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   root 'customers#index'
   resources :customers, only: [:index]
-  resources :administrators
+  resources :administrator
 end
