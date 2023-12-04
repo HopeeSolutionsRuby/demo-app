21ba6b026d5f6c41725c62c5780b2b2e7b782fb8
khoadev2512 <khoa.nguyencse.npng@hcmut.edu.vn>
2023-12-04 10:06:22 +0700

    Init layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :customers, only: [:index]
   root 'customers#index'
+  resources :customers, only: [:index]
+  resources :administrators
 end
