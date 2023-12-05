6b7a713d47fae23b5d3d26b6edbdd37283b4121c
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-05 16:50:29 +0700

    [issue_34]_modify css and responsive

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'jquery', to: 'jquery.min.js', preload: true
-pin 'popper', to: 'popper.js', preload: true
+pin '@popperjs/core', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery-ui", to: "jquery-ui.min.js", preload: true
