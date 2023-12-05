0f0f65b130b97c4998d9108626eea78503ace63d
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-05 17:24:35 +0700

    [issue_34]_modify js controller user

--- app/javascript/controllers/customer_controller.js
+++ app/javascript/controllers/customer_controller.js
@@ -4,11 +4,23 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     console.log("home controller has been connected");
-    $(window).on("mouseup", () => {
-      $(".popup-table").addClass("hidden-popup");
-    });
-    $("#popupTrigger").on("click", () => {
+    $("#popupTrigger").on("click", (e) => {
+      e.stopPropagation(); // Ngăn chặn sự kiện lan ra các phần tử cha
       $(".popup-table").toggleClass("hidden-popup");
     });
+    
+    $(window).on("mouseup", (event) => {
+      if (!$(".popup-table").hasClass("hidden-popup")) {
+        if (!event.target.closest("#popupTrigger")) {
+          $(".popup-table").addClass("hidden-popup");
+        }
+      }
+    });
+    
+    
+    
+    // $("#popupMore").on("click", () => {
+    //   $(".popup-more").toggleClass("hidden-popup");
+    // });
   }
 }
