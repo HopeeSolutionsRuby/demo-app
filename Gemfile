505e676a3b3e9ed1febe91f31f2e632aaee5c92c
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-06 15:35:33 +0700

    [issue_34]_fix debug issue and modify responsive

--- Gemfile
+++ Gemfile
@@ -96,4 +96,4 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
 end
-gem "font-awesome-sass", "~> 6.5.1"
+gem 'font-awesome-sass', '~> 6.5.1'
--- app/javascript/controllers/customer_controller.js
+++ app/javascript/controllers/customer_controller.js
@@ -6,11 +6,20 @@ export default class extends Controller {
       e.stopPropagation();
       $(".popup-table").toggleClass("hidden-popup");
     });
+    $("#more").on("click", (e) => {
+      e.stopPropagation();
+      $(".table-responsive").toggleClass("hidden-table");
+    });
 
     $(window).on("mouseup", (event) => {
       if (!$(".popup-table").hasClass("hidden-popup")) {
         if (!event.target.closest("#popupTrigger")) {
           $(".popup-table").addClass("hidden-popup");
+        }
+      }
+      if (!$(".table-responsive").hasClass("hidden-table")) {
+        if (!event.target.closest("#more")) {
+          $(".table-responsive").addClass("hidden-table");
         }
       }
     });
