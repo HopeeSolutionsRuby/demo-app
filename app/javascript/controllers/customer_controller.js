595505fb0804b90babc78ce11864efb5310a7723
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-06 10:11:25 +0700

    [issue_34]_remove debug issue

--- app/javascript/controllers/customer_controller.js
+++ app/javascript/controllers/customer_controller.js
@@ -2,12 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    console.log("home controller has been connected");
     $("#popupTrigger").on("click", (e) => {
-      e.stopPropagation(); 
+      e.stopPropagation();
       $(".popup-table").toggleClass("hidden-popup");
     });
-    
+
     $(window).on("mouseup", (event) => {
       if (!$(".popup-table").hasClass("hidden-popup")) {
         if (!event.target.closest("#popupTrigger")) {
@@ -15,6 +14,5 @@ export default class extends Controller {
         }
       }
     });
-    
   }
 }
