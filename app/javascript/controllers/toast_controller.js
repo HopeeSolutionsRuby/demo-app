d97af290583eb478f615ceb759ba2698976a4960
minhnguyennn <minh842657913@gmail.com>
2023-12-09 15:00:58 +0700

    [Issue 35]: Edit UI toast and dispose toast from DOM after hide

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -7,6 +7,22 @@ export default class extends Controller {
     // Show the toast
     const toast = new bootstrap.Toast(toastEl);
     toast.show();
-    toast.timeOut = setTimeout(() => toast.remove(), 5000 );
+
+    var displayTime = 5000;
+    var disposeTime = 500;
+    setTimeout(function () {
+      toast.hide();
+
+      setTimeout(() => {
+        toast.dispose();
+      }, disposeTime);
+    }, displayTime);
+
+    //Customs hiden the toast
+    // const custToast = document.getElementById("cus-toast");
+    // var displayTime = 5000;
+    // setTimeout(function () {
+    //   toast.hide();
+    // }, displayTime);
   }
 }
