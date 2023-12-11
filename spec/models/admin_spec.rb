98c5a55f38ba7f6c28e95bda21b6de2b200e42b2
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-11 13:08:21 +0700

    [issue_35]_fix style code

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -4,17 +4,14 @@ RSpec.describe Admin, type: :model do
   describe 'validations' do
     # Check username
     it { should validate_presence_of(:username) }
-
     # Check password
     it 'validates password format' do
       admin = Admin.create(username: 'test_admin', password: 'pass12')
-
       expect(admin.errors[:password]).not_to include('should contain at least one letter, one number, and be at least 6 characters long')
     end
 
     it 'validates password not format' do
       admin = Admin.create(username: 'test_admin', password: 'passss', email: 'dat@gmail.com')
-    
       expect(admin.errors.full_messages).to include('Password should contain at least one letter, one number, and be at least 6 characters long')
     end
   end
