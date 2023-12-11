ff201b6e4ade3e01012ff9c91175c7b186fe8b32
minhnguyennn <minh842657913@gmail.com>
2023-12-11 11:45:34 +0700

    [Issue 35]: Finish unit test model admin

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'shoulda/matchers'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
