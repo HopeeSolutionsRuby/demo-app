dde28b35796889c5a92454f7ee6e9e109a3a2b8a
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-12 17:47:50 +0700

    [issue_35]_fix bug

--- db/migrate/20231207102823_add_username_to_admins.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddUsernameToAdmins < ActiveRecord::Migration[7.1]
-  def change
-    add_column :admins, :username, :string
-    add_index :admins, :username, unique: true
-  end
-end
--- db/migrate/20231208015507_change_admin_name_to_user_name.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeAdminNameToUserName < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :admins, :admin_name, :username
-  end
-end
