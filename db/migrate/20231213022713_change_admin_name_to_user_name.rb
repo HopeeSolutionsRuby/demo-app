557efcf1c853c0b7f4014d9acb895a8e74dd5216
minhnguyennn <minh842657913@gmail.com>
2023-12-13 17:22:21 +0700

    [Issue 35]: Delete advise and fix migration

--- db/migrate/20231213022713_change_admin_name_to_user_name.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# AddUsernameToAdmins
-class AddUsernameToAdmins < ActiveRecord::Migration[7.1]
-  def change
-    add_column :admins, :username, :string
-    add_index :admins, :username, unique: true
-  end
-end
