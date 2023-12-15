df8555285966ee5a5fcbef85b45890a0c6078f3d
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-15 15:57:45 +0700

    [issue_45]_add image in edit clinic

--- app/controllers/administrator/clinics_controller.rb
+++ app/controllers/administrator/clinics_controller.rb
@@ -6,7 +6,7 @@ module Administrator
     include Pagy::Backend
     def index
       # @clinics = []
-      # 30.times do
+      # 100.times do
       #   clinic = FactoryBot.create(:clinic)
       #   @clinics << clinic
       # end
@@ -23,8 +23,10 @@ module Administrator
     def show
       @clinic = Clinic.find(params[:id])
     end
+
     def create
       # book_params.inspect
+
       @clinic = Clinic.new(clinic_params)
 
       respond_to do |format|
@@ -43,25 +45,39 @@ module Administrator
       @clinic = Clinic.find(params[:id])
     end
 
+    def destroy
+      @clinic = Clinic.find(params[:id])
+      return unless @clinic.destroy
+
+      redirect_to administrator_clinics_path
+    end
+
     def update
       @clinic = Clinic.find(params[:id])
-      if @clinic.update(clinic_params)
+
+      if params[:pictures].present?
+        existing_pictures = @clinic.pictures || []
+        new_pictures = Array(params[:pictures].values)
+        updated_pictures = existing_pictures + new_pictures
+      else
+        updated_pictures = @clinic.pictures
+      end
+
+      if @clinic.update(clinic_params.merge(pictures: updated_pictures))
         redirect_to administrator_clinics_path
       else
         render :edit
       end
     end
-    def destroy 
-      @clinic = Clinic.find(params[:id])
-      if @clinic.destroy
-        redirect_to administrator_clinics_path
-      end
-    end
-    
+
     private
 
     def clinic_params
-      params.require(:clinic).permit(:id, :name, :address, :region, :faculity, pictures: [])
+      if params[:pictures].present?
+        params.permit(:id).merge(pictures: params[:pictures].values)
+      else
+        params.require(:clinic).permit(:id, :name, :address, :region, :faculity, :pictures)
+      end
     end
 
     def search_params
--- db/migrate/20231212033639_create_clinics.rb
+++ db/migrate/20231212033639_create_clinics.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# CreateClinics
 class CreateClinics < ActiveRecord::Migration[7.1]
   def change
     create_table :clinics do |t|
