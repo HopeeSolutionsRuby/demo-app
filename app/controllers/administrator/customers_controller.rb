8831b46b39314f8f1168708613d801855c60be04
minhnguyennn <minh842657913@gmail.com>
2023-12-15 10:25:54 +0700

    [Issue 44]: Create model delete customer

--- app/controllers/administrator/customers_controller.rb
+++ app/controllers/administrator/customers_controller.rb
@@ -3,16 +3,23 @@
 module Administrator
   # class Dashboard Controller
   class CustomersController < BaseController
-    before_action :set_customer, only: %i[show]
-    # GET /customers
+    before_action :set_customer, only: %i[show destroy]
     def index
       @customers = Customer.all
     end
     def show; end
+    def destroy
+      
+      @customer.destroy
+      flash[:alert] = "Customer has been deleted successfully."
+      redirect_to administrator_dashboard_index_path
+    end
   
     protected
     def set_customer
+      puts "===========#{@customer}============="
       @customer = Customer.find_by(id: params[:id])
+     
       unless @customer
         flash[:alert] = "Customer with ID #{params[:id]} not found."
         redirect_to administrator_dashboard_index_path
