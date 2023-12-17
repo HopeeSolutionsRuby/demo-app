832dc3977d2105a58e2af51eceba6d0c15bcdb3c
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-17 18:18:11 +0700

    [issue_45]_drag and drop final

--- app/controllers/administrator/clinics_controller.rb
+++ app/controllers/administrator/clinics_controller.rb
@@ -24,11 +24,7 @@ module Administrator
       @clinic = Clinic.find(params[:id])
     end
     def create
-      # book_params.inspect
-   
-
       @clinic = Clinic.new(clinic_params)
-
       respond_to do |format|
         if @clinic.save
           format.html { redirect_to '/administrator/clinics', notice: 'Clinic was successfully created.' }
@@ -44,42 +40,28 @@ module Administrator
     def edit
       @clinic = Clinic.find(params[:id])
     end
-    def destroy 
+
+    def destroy
       @clinic = Clinic.find(params[:id])
       if @clinic.destroy
         redirect_to administrator_clinics_path
       end
     end
-    
+
     def update
       @clinic = Clinic.find(params[:id])
-    
-      if params[:pictures].present?
-        existing_pictures = @clinic.pictures || []
-        new_pictures = Array(params[:pictures].values)
-        updated_pictures = existing_pictures + new_pictures
-      else
-        updated_pictures = @clinic.pictures
-      end
-    
-      if @clinic.update(clinic_params.merge(pictures: updated_pictures))
+      if @clinic.update(clinic_params)
         redirect_to administrator_clinics_path
       else
         render :edit
       end
     end
-    
-    
+
     private
 
     def clinic_params
-      if params[:pictures].present?
-        params.permit(:id).merge(pictures: params[:pictures].values)
-      else
-        params.require(:clinic).permit(:id, :name, :address, :region, :faculity,:pictures)
-      end
+      params.require(:clinic).permit(:id, :name, :address, :region, :faculity, pictures: [])
     end
-    
 
     def search_params
       params.fetch(:query, {}).permit!
