b3df7be12ff0f186b55c9143767dabb3c07fcc43
HoThanhDat <hothanhdatsd@gmail.com>
2023-12-20 08:51:24 +0700

    [issue_45]_add icon remove picture when update and create

--- app/javascript/controllers/dropzone_controller.js
+++ app/javascript/controllers/dropzone_controller.js
@@ -7,10 +7,6 @@ export default class extends Controller {
   connect() {
     this.inputTarget.addEventListener("change", this.previewImages.bind(this));
     this.inputTarget.addEventListener("input", this.handleInput.bind(this));
-    this.previewContainerTarget.addEventListener(
-      "click",
-      this.deleteImage.bind(this)
-    );
   }
 
   handleInput() {
@@ -18,9 +14,13 @@ export default class extends Controller {
       $(".drag").removeClass("d-none");
       const preview = this.previewContainerTarget;
       const existingImages = preview.querySelectorAll(".preview-image");
+      const existingIcons = preview.querySelectorAll(".close-icon");
       existingImages.forEach((image) => {
         URL.revokeObjectURL(image.src);
         image.remove();
+      });
+      existingIcons.forEach((icon) => {
+        icon.remove();
       });
     }
   }
@@ -31,27 +31,45 @@ export default class extends Controller {
       $(".drag").addClass("d-none");
       const preview = this.previewContainerTarget;
       const existingImages = preview.querySelectorAll(".preview-image");
+      const existingIcons = preview.querySelectorAll(".close-icon");
       existingImages.forEach((image) => {
         image.remove();
+      });
+      existingIcons.forEach((icon) => {
+        icon.remove();
       });
       this.handleInput();
       for (let i = 0; i < files.length; i++) {
         const file = files[i];
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("image-container");
         const img = document.createElement("img");
         img.src = URL.createObjectURL(file);
         img.classList.add("preview-image");
         img.setAttribute("data-name", file.name);
-        preview.appendChild(img);
+        imgContainer.appendChild(img);
+
+        const closeIcon = document.createElement("span");
+        closeIcon.innerHTML = "&times;";
+        closeIcon.classList.add("close-icon");
+        closeIcon.addEventListener("click", this.deleteImage.bind(this));
+        imgContainer.appendChild(closeIcon);
+
+        preview.appendChild(imgContainer);
       }
     }
   }
 
   deleteImage(event) {
-    if (event.target.classList.contains("preview-image")) {
+    if (event.target.classList.contains("close-icon")) {
       const confirmed = confirm("Are you sure you want to delete this image?");
       if (confirmed) {
-        event.target.remove();
-        const fileToRemove = event.target.getAttribute("data-name");
+        const imageContainer = event.target.parentElement;
+        const fileToRemove = imageContainer
+          .querySelector(".preview-image")
+          .getAttribute("data-name");
+
+        imageContainer.remove();
         const files = Array.from(this.inputTarget.files);
         const updatedFiles = files.filter((file) => {
           return file.name !== fileToRemove;
