fa01e6d684e8ce8452693ae709c89e2835436d90
minhnguyennn <minh842657913@gmail.com>
2023-12-20 17:57:15 +0700

    [Issue 44]: Fix UI admin customer

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -4,4 +4,8 @@ Ransack.configure do |c|
   # Change default search parameter key name.
   # Default key name is :q
   c.search_key = :query
+  c.custom_arrows = {
+    up_arrow: '<i class="fa-solid fa-sort" style="font-size: 12px; margin-left: 5px; color: #9299AC;"></i>',
+    down_arrow: '<i class="fa-solid fa-sort" style="font-size: 12px; margin-left: 5px; color: #9299AC;"></i>'
+  }
 end
