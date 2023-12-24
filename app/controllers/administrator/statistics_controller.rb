e2c9322303690e10c7ffd68cd1a35faa863757d2
Hoan <phamphihoan205@gmail.com>
2023-12-24 22:43:54 +0700

    [issue_46] fix default active

--- app/controllers/administrator/statistics_controller.rb
+++ app/controllers/administrator/statistics_controller.rb
@@ -1,39 +1,37 @@
 # frozen_string_literal: true
 
 module Administrator
-  # Path: app/controllers/administrator/statistics_controller.rb
   class StatisticsController < BaseController
     include Pagy::Backend
 
     def index
       filter_month = params[:filter_month]
-      Rails.logger.debug "Params: #{params.inspect}"
 
-
-      @selected_month = filter_month.present? ? filter_month.split('/') : %w[2023 12]
-      Rails.logger.debug "Params: #{@selected_month}"
-
+      @selected_month = filter_month.present? ? filter_month.split('/') : %w[2023 1]
       return if @selected_month.blank?
-      @year, @month = @selected_month.map(&:to_i)
 
+      @year, @month = @selected_month.map(&:to_i)
       @month = 1 unless (1..12).include?(@month)
-      Rails.logger.debug "Params: #{@month}"
 
       start_date = Date.new(@year, @month, 1)
       end_date = start_date.end_of_month
 
       sign_ups_by_day = Customer.where(created_at: start_date..end_date).group("DATE_FORMAT(created_at, '%d/%m')").count
-      @sign_ups_by_day = (start_date..end_date).map do |date|
-        formatted_key = date.strftime('%d/%m')
-        [formatted_key, sign_ups_by_day[formatted_key] || 0]
-      end.to_h
-        Rails.logger.debug "data: #{@sign_ups_by_day}"
-
+      @sign_ups_by_day = generate_sign_up_data(start_date, end_date, sign_ups_by_day)
 
       respond_to do |format|
         format.html
         format.json { render json: @sign_ups_by_day }
       end
+    end
+
+    private
+
+    def generate_sign_up_data(start_date, end_date, sign_ups_by_day)
+      (start_date..end_date).map do |date|
+        formatted_key = date.strftime('%d/%m')
+        [formatted_key, sign_ups_by_day[formatted_key] || 0]
+      end.to_h
     end
   end
 end
--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,13 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    $(document).ready(function() {
+    $(".datepicker .active").removeClass("active");
+});
+
     $('#filter_month').datepicker({
-      format: "yyyy/mm",
+      format: "yyyy-mm",
       viewMode: "months", 
       minViewMode: "months"
     }).on('changeDate', function(e) {
         const selectedDate = e.date;
-        const filterMonth = `${selectedDate.getFullYear()}/${String(selectedDate.getMonth() + 1)}`;
+        const filterMonth = `${selectedDate.getFullYear()}/${String(selectedDate.getMonth() + 1).padStart(2, '0')}`;
         
         window.location.href = "/administrator/statistics?filter_month=" + filterMonth;     
     });
